fb0d5fbc6eca11bdc4015196ad46bed3dc91be7f
JpEscobarM <jp.escobarmartins@gmail.com>
2025-06-23 15:03:20 -0300

    update controller,service,repository
    added route to create product

--- product.service/src/main/java/com/techie/microservices/product/controller/ProductController.java
+++ product.service/src/main/java/com/techie/microservices/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,15 +9,24 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techie.microservices.product.dto.ProductRequest;
+import com.techie.microservices.product.model.Product;
+import com.techie.microservices.product.service.ProductService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void createProduct(@RequestBody ProductRequest productRequest) {
+	public Product createProduct(@RequestBody ProductRequest productRequest) {
 		
+		return productService.createProduct(productRequest);
 	}
 	
 }
--- product.service/src/main/java/com/techie/microservices/product/service/ProductService.java
+++ product.service/src/main/java/com/techie/microservices/product/service/ProductService.java
@@ -1,5 +1,39 @@
 package com.techie.microservices.product.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.techie.microservices.product.dto.ProductRequest;
+import com.techie.microservices.product.model.Product;
+import com.techie.microservices.product.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public Product createProduct(ProductRequest prodRequest) {
+	//Product p = new Product(prodRequest.id(),prodRequest.name(),prodRequest.description(),prodRequest.price());
+		
+		Product product = Product.builder()
+			    .name(prodRequest.name())
+			    .description(prodRequest.description())
+			    .price(prodRequest.price())
+			    .build();
+		
+		productRepository.save(product);
+		
+		log.info("Product created successfully");
+		return product;
+	}
+	
+	
+	
+	
 }
